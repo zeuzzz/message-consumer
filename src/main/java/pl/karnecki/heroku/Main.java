c58521742322f024fe864f64d01ed202dba57ed4
skarnecki <szymon.karnecki@gmail.com>
2015-04-27 23:25:50 +0200

    Main class update

--- src/main/java/pl/karnecki/heroku/Main.java
+++ src/main/java/pl/karnecki/heroku/Main.java
@@ -1,30 +1,26 @@
 package pl.karnecki.heroku;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
+
 public class Main {
 
-//    public static void main(String[] args) throws Exception {
-//        Server server = new Server(8081);
-//        ServletContextHandler handler = new ServletContextHandler();
-//        handler.setContextPath("");
-//        handler.addServlet(new ServletHolder(new ServletContainer(resourceConfig())), "/*");
-//        server.setHandler(handler);
-//        try {
-//            server.start();
-//        } catch (Exception e) {
-//            throw new RuntimeException("Could not start the server", e);
-//        }
-//    }
-//
-//    private static ResourceConfig resourceConfig() throws URISyntaxException, SQLException {
-//        DataSourceProvider dataSourceProvider = new DataSourceProvider();
-//        MessageDao messageDao = new MessageDao();
-//        messageDao.setDataSource(dataSourceProvider.getDataSource());
-//        MessageEndpoint messageEndpoint = new MessageEndpoint();
-//        messageEndpoint.setMessageDao(messageDao);
-//        ResourceConfig resourceConfig = new ResourceConfig();
-//        resourceConfig.register(MoxyJsonFeature.class);
-//        resourceConfig.property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
-//
-//        return resourceConfig.register(messageEndpoint);
-//    }
+    public static void main(String[] args) throws Exception {
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        Server server = new Server(Integer.valueOf(webPort));
+        ServletContextHandler handler = new ServletContextHandler();
+        handler.setContextPath("");
+        handler.addServlet(new ServletHolder(new ServletContainer(new DropResource())), "/*");
+        server.setHandler(handler);
+        try {
+            server.start();
+        } catch (Exception e) {
+            throw new RuntimeException("Could not start the server", e);
+        }
+    }
 }
